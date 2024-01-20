b7203f706d8af51804ddae5c2c75a2002525171c
yaminabes <yamina.ouadah@utbm.fr>
2024-01-20 13:21:53 +0000

    added developper filters by responsible

--- project/src/DataFixtures/UserDataFixtures.php
+++ project/src/DataFixtures/UserDataFixtures.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\DataFixtures;
 
 use App\Entity\User;
@@ -17,13 +18,40 @@ class UserDataFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $firstNames = ['John', 'Jane', 'Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Henry'];
-        $lastNames = ['Doe', 'Smith', 'Johnson', 'Brown', 'Lee', 'Wilson', 'Evans', 'Moore', 'Wang', 'Baker'];
+        $admin = new User();
+        $admin->setFirstName('Admin');
+        $admin->setLastName('User');
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setBasicCost(rand(5000, 50000));
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'toor'));
+        $manager->persist($admin);
+
+        $responsible1 = new User();
+        $responsible1->setFirstName('Yamina');
+        $responsible1->setLastName('OUADAH');
+        $responsible1->setEmail('yamina@example.com');
+        $responsible1->setRoles(['ROLE_RESPONSABLE']);
+        $responsible1->setBasicCost(rand(5000, 50000));
+        $responsible1->setPassword($this->passwordEncoder->encodePassword($responsible1, 'toor'));
+        $manager->persist($responsible1);
+
+        $responsible2 = new User();
+        $responsible2->setFirstName('Hajar');
+        $responsible2->setLastName('BENNIS');
+        $responsible2->setEmail('hajar@example.com');
+        $responsible2->setRoles(['ROLE_RESPONSABLE']);
+        $responsible2->setBasicCost(rand(5000, 50000));
+        $responsible2->setPassword($this->passwordEncoder->encodePassword($responsible2, 'toor'));
+        $manager->persist($responsible2);
+
+        $firstNames = ['Yamina', 'Hajar', 'Ridvan', 'Furkan', 'Sofiane', 'David', 'Eva', 'Paul', 'Julie', 'Manon'];
+        $lastNames = ['Bouaziz', 'Benchekroun', 'Kerbouche', 'Lefevre', 'Renard', 'Boudjemaa', 'Dupont', 'Moreau', 'Lamrani', 'Bensaïd'];
 
         $usedFirstNames = [];
         $usedLastNames = [];
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 2; $i < 12; $i++) {
             $firstName = $this->getUniqueValue($firstNames, $usedFirstNames);
             $lastName = $this->getUniqueValue($lastNames, $usedLastNames);
 
@@ -32,9 +60,14 @@ class UserDataFixtures extends Fixture
             $user->setLastName($lastName);
             $user->setEmail('user' . $i . '@example.com');
             $user->setRoles(['ROLE_USER']);
-            $user->setBasicCost(rand(5000, 50000)); 
-
+            $user->setBasicCost(rand(5000, 50000));
             $user->setPassword($this->passwordEncoder->encodePassword($user, 'password'));
+
+            if (rand(0, 1) == 1) {
+                $user->setResponsable($responsible1);
+            } else {
+                $user->setResponsable($responsible2);
+            }
 
             $manager->persist($user);
         }
